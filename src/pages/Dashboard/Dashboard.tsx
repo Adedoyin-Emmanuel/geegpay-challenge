5aec355b0b270db2df6627758faf0e84ef36a8c2
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-01-27 16:46:39 +0100

    Feat: Finished Cards

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -3,17 +3,22 @@ import SideNav from "../../components/SideNav/SideNav";
 import SalesTrend from "../../components/SalesTrend/SalesTrend";
 import Card from "../../components/Card/Card";
 import BoxTickIcon from "./../../assets/icons/box-icon-1.svg";
+import Box3DRotateIcon from "./../../assets/icons/box-icon-2.svg";
 import GreenGraphIcon from "./../../assets/icons/good-graph.svg";
+import BadGraphIcon from "./../../assets/icons/bad-graph.svg";
 import GreenTrendIcon from "./../../assets/icons/trending-up-icon.svg";
+import BadTrendIcon from "./../../assets/icons/trending-down-icon.svg";
+import ShoppingIcon from "./../../assets/icons/cart-icon.svg";
+import DollarIcon from "./../../assets/icons/coin-icon.svg";
 
 const Dashboard = () => {
   return (
     <div className="w-screen h-full flex">
       <SideNav />
       <DashboardHeader>
-        <section className="w-full md:flex gap-6">
+        <section className="w-full md:flex gap-6 justify-between">
           <SalesTrend />
-          <section className="my-5 md:my-0 p-2">
+          <section className="w-full flex md:flex-row flex-col flex-wrap md:items-center md:justify-between  gap-5 md:gap-10 my-5 md:my-0 p-2">
             <Card>
               <section className="card-header flex items-start justify-between">
                 <img src={BoxTickIcon} alt="Box Tick" />
@@ -37,7 +42,101 @@ const Dashboard = () => {
                     alt="Green trend"
                     className="h-3 w-3"
                   />
-                  <p className="text-[12px] text-[#34CAA5]">23,5%</p>
+                  <p className="text-[12px] text-[#34CAA5]">23.5%</p>
+                </div>
+
+                <div className="flex">
+                  <p className="text-[#606060] text-[14px]">
+                    vs . previous month
+                  </p>
+                </div>
+              </section>
+            </Card>
+
+            <Card>
+              <section className="card-header flex items-start justify-between">
+                <img src={Box3DRotateIcon} alt="Box Tick" />
+                <img src={BadGraphIcon} alt="Bad Graph" />
+              </section>
+
+              <h3 className="capitalize  text-[#898989] text-[18px] font-medium">
+                total refund
+              </h3>
+              <h1 className="text-2xl font-[600]  text-[#3A3F51]">270</h1>
+
+              <section className="flex items-start justify-between">
+                <div
+                  className="rounded-[1000px] flex items-center justify-between p-2 px-4  h-[24px]"
+                  style={{
+                    background: "rgba(237, 84, 78, 0.12)",
+                  }}
+                >
+                  <img src={BadTrendIcon} alt="Red trend" className="h-3 w-3" />
+                  <p className="text-[12px] text-[#ED544E]">23.5%</p>
+                </div>
+
+                <div className="flex">
+                  <p className="text-[#606060] text-[14px]">
+                    vs . previous month
+                  </p>
+                </div>
+              </section>
+            </Card>
+
+            <Card>
+              <section className="card-header flex items-start justify-between">
+                <img src={ShoppingIcon} alt="Shopping icon" />
+                <img src={BadGraphIcon} alt="Bad Graph" />
+              </section>
+
+              <h3 className="capitalize  text-[#898989] text-[18px] font-medium">
+                average sales
+              </h3>
+              <h1 className="text-2xl font-[600]  text-[#3A3F51]">1576</h1>
+
+              <section className="flex items-start justify-between">
+                <div
+                  className="rounded-[1000px] flex items-center justify-between p-2 px-4  h-[24px]"
+                  style={{
+                    background: "rgba(237, 84, 78, 0.12)",
+                  }}
+                >
+                  <img src={BadTrendIcon} alt="Red trend" className="h-3 w-3" />
+                  <p className="text-[12px] text-[#ED544E]">23.5%</p>
+                </div>
+
+                <div className="flex">
+                  <p className="text-[#606060] text-[14px]">
+                    vs . previous month
+                  </p>
+                </div>
+              </section>
+            </Card>
+
+            <Card className="transform md:translate-y-2">
+              <section className="card-header flex items-start justify-between">
+                <img src={DollarIcon} alt="Coin icon" />
+                <img src={GreenGraphIcon} alt="Green Graph" />
+              </section>
+
+              <h3 className="capitalize  text-[#898989] text-[18px] font-medium">
+                total income
+              </h3>
+              <h1 className="text-2xl font-[600]  text-[#3A3F51]">$350.00</h1>
+
+              <section className="flex items-start justify-between">
+                <div
+                  className="rounded-[1000px] flex items-center justify-between p-2 px-4  h-[24px]"
+                  style={{
+                    background: "rgba(52, 202, 165, 0.10)",
+                  }}
+                >
+                  <img
+                    src={GreenTrendIcon}
+                    alt="Green trend"
+                    className="h-3 w-3"
+                  />
+                  <p className="text-[12px] text-[#34CAA5]">23.5%</p>
                 </div>
 
                 <div className="flex">
